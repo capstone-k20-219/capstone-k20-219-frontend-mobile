cbebdeabb10a52b6772b45ce57be31b210abce7d
Huu Duc <huuduc2707@gmail.com>
2024-01-08 18:43:21 +0700

    feat(Components): add components

---
+++ app/components/VerticalSeparator.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { colors, typography } from "app/theme"
+import { Text } from "app/components/Text"
+
+export interface VerticalSeparatorProps {
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: StyleProp<ViewStyle>
+}
+
+/**
+ * Describe your component here
+ */
+export const VerticalSeparator = observer(function VerticalSeparator(
+  props: VerticalSeparatorProps,
+) {
+  const { style } = props
+  const $styles = [$container, style]
+
+  return (
+    <View style={$styles}>
+      <Text style={$text}>Hello</Text>
+    </View>
+  )
+})
+
+const $container: ViewStyle = {
+  justifyContent: "center",
+}
+
+const $text: TextStyle = {
+  fontFamily: typography.primary.normal,
+  fontSize: 14,
+  color: colors.palette.primary500,
+}
